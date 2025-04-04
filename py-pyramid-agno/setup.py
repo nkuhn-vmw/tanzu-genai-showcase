c1e154a0fa1287d1311192a852e78eae8788c0cb
Chris Phillipson <cphillipson@pivotal.io>
2025-04-03 19:44:58 -0700

    Update version; added patch version placeholder

--- py-pyramid-agno/setup.py
+++ py-pyramid-agno/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airbnb-assistant',
-    version='0.1',
+    version='0.1.0',
     description='Airbnb Search Assistant using Pyramid and Agno',
     author='',
     author_email='',
